06ad628a10cc61f21c4f5bd024a2b761f7292d97
ilknurs <ilknursercek4@gmail.com>
2023-06-05 22:32:00 +0300

    Solidity ERC20 Token Course

---
+++ .learneth/Solidity ERC20 Token Course/1. Introduction/introduction.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+
+// Question 1:
+// Why are blockchain based tokens so revolutionary?
+
+// Because people can now make investments anonymously.
+// Because they represent ownership in digital assets that can be owned and transferred.
+// Because you can use tokens to make transactions without having to pay taxes.
+
+
+// Question 2:
+// Why did the community create token standards?
+
+// So that the community can control and approve the tokens that are created.
+// In order to restrict the functionality of tokens to safe and non-malicious actions.
+// So that the community can create tokens that are interoperable with other contracts, products, and services.
+
+
+// Question 3:
+// If you would create a decentralised application for a baseball trading card game where each baseball player would be represented by a token, what token standard would you use to write the token contract?
+
+// ERC20
+// ERC721
+
+contract Quiz {
+    uint8 public question1 = 2;
+    uint8 public question2 = 3;
+    uint8 public question3 = 2;
+}
